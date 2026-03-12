830ff4d6b6248bfc1436bdb427a6659edf31740e
root <root@srv1476596.hstgr.cloud>
2026-03-12 16:19:31 +0000

    fix: duplicate.js import path correto (4 niveis)

--- pages/api/projects/[id]/duplicate.js
+++ pages/api/projects/[id]/duplicate.js
@@ -1,4 +1,4 @@
-import { initDatabase, getProjectById, createProject } from '../../../../../lib/database'
+import { initDatabase, getProjectById, createProject } from ../../../../lib/database
 
 let dbInitialized = false;
 async function ensureDb() {
